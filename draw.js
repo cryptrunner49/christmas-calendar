c6c6d11f49ccc9c603798b1c1339b65cf1fcaaea
cryptrunner49 <cryptrunner49@proton.me>
2024-12-27 20:06:32 +0000

    Christmas calendar

---
+++ draw.js
@@ -0,0 +1,40 @@
+const draw = {};
+
+draw.circle = (ctx, x, y, radius, options) => {
+    ctx.beginPath();
+
+    if (options.outline == "inside") {
+        radius -= options.lineWidth / 2;
+    }
+
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    Object.assign(ctx, options);
+
+    options.fillStyle && ctx.fill();
+    options.strokeStyle && ctx.stroke();
+}
+
+draw.ellipse = (ctx, x, y, xRadius, yRadius, options) => {
+    ctx.beginPath();
+    ctx.ellipse(x, y, xRadius, yRadius, 0, 0, Math.PI * 2);
+    Object.assign(ctx, options);
+
+    options.fillStyle && ctx.fill();
+    options.strokeStyle && ctx.stroke();
+}
+
+draw.line = (ctx, fromX, fromY, toX, toY, options) => {
+    ctx.beginPath();
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(toX, toY);
+    Object.assign(ctx, options);
+    ctx.stroke();
+};
+
+const color = {};
+color.darkest = (hue) => `hsl(${hue}, 100%, 15%)`;
+color.dark = (hue) => `hsl(${hue}, 100%, 30%)`;
+color.normal = (hue) => `hsl(${hue}, 100%, 50%)`;
+color.light = (hue) => `hsl(${hue}, 100%, 70%)`;
+color.lightest = (hue) => `hsl(${hue}, 100%, 85%)`;
+color.reverse = (hue) => (hue + 180) % 360;
---
+++ temp.js
@@ -0,0 +1,6 @@
+function drawBall(ctx, x, y, size, hue) {
+    const top = y - size / 2;
+    const left = x - size / 2;
+    const bottom = y + size / 2;
+    ctx.strokeRect(top, left, size, size);
+}
